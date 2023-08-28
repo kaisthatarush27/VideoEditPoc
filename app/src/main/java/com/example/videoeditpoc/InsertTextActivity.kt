6e82e4a34622ed183f3a81f1c7e2524ac368ac41
diatozian <weewoo@diatoz.com>
2023-08-28 08:13:19 +0530

    fixed insert text functionality.

--- app/src/main/java/com/example/videoeditpoc/InsertTextActivity.kt
+++ app/src/main/java/com/example/videoeditpoc/InsertTextActivity.kt
@@ -2,24 +2,23 @@ package com.example.videoeditpoc
 
 import android.app.ProgressDialog
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.arthenica.ffmpegkit.FFmpegKit
 import com.arthenica.ffmpegkit.FFmpegKitConfig
 import com.example.videoeditpoc.databinding.ActivityInsertTextBinding
-import com.example.videoeditpoc.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.io.File
 import java.io.FileInputStream
+import java.io.IOException
 import java.io.InputStream
-import kotlin.math.abs
 
 class InsertTextActivity : AppCompatActivity() {
     lateinit var binding: ActivityInsertTextBinding
@@ -30,14 +29,14 @@ class InsertTextActivity : AppCompatActivity() {
         binding = ActivityInsertTextBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.insertText.setOnClickListener {
+        binding.insertTextBtn.setOnClickListener {
             if (input_video_uri_ffmpeg != null) {
                 insertTextUsingFfmpeg()
             } else Toast.makeText(this, "Please upload video", Toast.LENGTH_LONG)
                 .show()
         }
 
-        binding.selectVideoUsingFfmpeg.setOnClickListener {
+        binding.selectVideoBtn.setOnClickListener {
             handler.removeCallbacksAndMessages(null)
             selectVideoLauncherUsingFfmpeg.launch("video/*")
         }
@@ -50,54 +49,54 @@ class InsertTextActivity : AppCompatActivity() {
                 .show()
         }
 
-        binding.insertGraphicActivity.setOnClickListener {
+        binding.insertGraphicActivityBtn.setOnClickListener {
             startActivity(Intent(this, InsertGraphicActivity::class.java))
         }
 
-        binding.videoView.setOnPreparedListener { mp ->
+//        binding.videoView.setOnPreparedListener { mp ->
 //            get the duration of the video
-            val duration = mp.duration / 1000
-            //initially set the left TextView to "00:00:00"
-            binding.textleft.text = "00:00:00"
-            //initially set the right Text-View to the video length
-            //the getTime() method returns a formatted string in hh:mm:ss
-            binding.textright.text = getTime(mp.duration / 1000)
-            //this will run he video in loop i.e. the video won't stop
-            //when it reaches its duration
-            mp.isLooping = true
-
-            //set up the initial values of binding.rangeSeekBar
-            binding.rangeSeekBar.setRangeValues(0, duration)
-            binding.rangeSeekBar.selectedMinValue = 0
-            binding.rangeSeekBar.selectedMaxValue = duration
-            binding.rangeSeekBar.isEnabled = true
-            binding.rangeSeekBar.setOnRangeSeekBarChangeListener { bar, minValue, maxValue ->
-                //we seek through the video when the user drags and adjusts the seekbar
-                binding.videoView.seekTo(minValue as Int * 1000)
-                //changing the left and right TextView according to the minValue and maxValue
-                binding.textleft.text = getTime(bar.selectedMinValue.toInt())
-                binding.textright.text = getTime(bar.selectedMaxValue.toInt())
-            }
-
-            //this method changes the right TextView every 1 second as the video is being played
-            //It works same as a time counter we see in any Video Player
-
-            handler.postDelayed(object : Runnable {
-                override fun run() {
-
-                    val time: Int = abs(duration - binding.videoView.currentPosition) / 1000
-                    binding.textleft.text = getTime(time)
-
-                    //wrapping the video, i.e. once the video reaches its length,
-                    // again starts from the current position of left seekbar point
-                    if (binding.videoView.currentPosition >= binding.rangeSeekBar.selectedMaxValue.toInt() * 1000) {
-                        binding.videoView.seekTo(binding.rangeSeekBar.selectedMinValue.toInt() * 1000)
-                    }
-                    handler.postDelayed(this, 1000)
-                }
-            }, 0)
-
-        }
+//            val duration = mp.duration / 1000
+//            //initially set the left TextView to "00:00:00"
+//            binding.textleft.text = "00:00:00"
+//            //initially set the right Text-View to the video length
+//            //the getTime() method returns a formatted string in hh:mm:ss
+//            binding.textright.text = getTime(mp.duration / 1000)
+//            //this will run he video in loop i.e. the video won't stop
+//            //when it reaches its duration
+//            mp.isLooping = true
+//
+//            //set up the initial values of binding.rangeSeekBar
+//            binding.rangeSeekBar.setRangeValues(0, duration)
+//            binding.rangeSeekBar.selectedMinValue = 0
+//            binding.rangeSeekBar.selectedMaxValue = duration
+//            binding.rangeSeekBar.isEnabled = true
+//            binding.rangeSeekBar.setOnRangeSeekBarChangeListener { bar, minValue, maxValue ->
+//                //we seek through the video when the user drags and adjusts the seekbar
+//                binding.videoView.seekTo(minValue as Int * 1000)
+//                //changing the left and right TextView according to the minValue and maxValue
+//                binding.textleft.text = getTime(bar.selectedMinValue.toInt())
+//                binding.textright.text = getTime(bar.selectedMaxValue.toInt())
+//            }
+//
+//            //this method changes the right TextView every 1 second as the video is being played
+//            //It works same as a time counter we see in any Video Player
+//
+//            handler.postDelayed(object : Runnable {
+//                override fun run() {
+//
+//                    val time: Int = abs(duration - binding.videoView.currentPosition) / 1000
+//                    binding.textleft.text = getTime(time)
+//
+//                    //wrapping the video, i.e. once the video reaches its length,
+//                    // again starts from the current position of left seekbar point
+//                    if (binding.videoView.currentPosition >= binding.rangeSeekBar.selectedMaxValue.toInt() * 1000) {
+//                        binding.videoView.seekTo(binding.rangeSeekBar.selectedMinValue.toInt() * 1000)
+//                    }
+//                    handler.postDelayed(this, 1000)
+//                }
+//            }, 0)
+//
+//        }
     }
 
 
@@ -139,22 +138,25 @@ class InsertTextActivity : AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.GetContent()) {
             it?.let {
                 input_video_uri_ffmpeg = FFmpegKitConfig.getSafParameterForRead(this, it)
-                binding.videoView.setVideoURI(it)
 
-                //after successful retrieval of the video and properly setting up the retried video uri in
-                //VideoView, Start the VideoView to play that video
-                binding.videoView.start()
+                Toast.makeText(
+                    this,
+                    "video loaded successfully: $input_video_uri_ffmpeg",
+                    Toast.LENGTH_SHORT
+                ).show()
+
             }
         }
 
     private fun insertTextUsingFfmpeg() {
-        val folder = cacheDir
-        val file = File(folder, System.currentTimeMillis().toString() + ".mp4")
+        val newFilePath: String = createExternalCacheFile(
+            System.currentTimeMillis().toString() + ".mp4"
+        ).absolutePath
         val fontFile = "/system/fonts/Roboto-Regular.ttf"
 
         val exe =
-            "-i $input_video_uri_ffmpeg -vf \"drawtext=text='© Krishna':fontfile='$fontFile':x=(main_w-text_w-10):y=(main_h-text_h-10):fontsize=100:fontcolor=black:box=1:boxcolor=white@0.5:boxborderw=5\" ${file.absolutePath}"
-        executeFfmpegCommand(exe, file.absolutePath)
+            "-y -i $input_video_uri_ffmpeg -vf \"drawtext=text='© Krishna':fontfile='$fontFile':x=(main_w-text_w-10):y=(main_h-text_h-10):fontsize=100:fontcolor=black:box=1:boxcolor=white@0.5:boxborderw=5\" $newFilePath"
+        executeFfmpegCommand(exe, newFilePath)
     }
 
     private fun executeFfmpegCommand(exe: String, filePath: String) {
@@ -177,14 +179,14 @@ class InsertTextActivity : AppCompatActivity() {
                 if (returnCode.isValueSuccess) {
                     //after successful execution of ffmpeg command,
                     //again set up the video Uri in VideoView
-                    binding.videoView.setVideoPath(filePath)
+//                    binding.videoView.setVideoPath(filePath)
                     //change the video_url to filePath, so that we could do more manipulations in the
                     //resultant video. By this we can apply as many effects as we want in a single video.
                     //Actually there are multiple videos being formed in storage but while using app it
                     //feels like we are doing manipulations in only one video
                     input_video_uri_ffmpeg = filePath
                     //play the result video in VideoView
-                    binding.videoView.start()
+//                    binding.videoView.start()
                     progressDialog.dismiss()
                     Toast.makeText(this@InsertTextActivity, "Filter Applied", Toast.LENGTH_SHORT)
                         .show()
@@ -204,5 +206,13 @@ class InsertTextActivity : AppCompatActivity() {
                 progressDialog.setMessage("Applying Filter..${log.message}")
             }
         }) { statistics -> Log.d("STATS", statistics.toString()) }
+    }
+
+    @Throws(IOException::class)
+    private fun createExternalCacheFile(fileName: String): File {
+        val file = File(externalCacheDir, fileName)
+        check(!(file.exists() && !file.delete())) { "Could not delete the previous export output file" }
+        check(file.createNewFile()) { "Could not create the export output file" }
+        return file
     }
 }
