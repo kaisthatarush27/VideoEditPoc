53c1a49268cca4279af4f3e82e6ad019df00925e
diatozian <weewoo@diatoz.com>
2023-09-06 15:12:15 +0530

    fixed resizing image functionality.

--- app/src/main/java/com/example/videoeditpoc/InsertPictureActivity.kt
+++ app/src/main/java/com/example/videoeditpoc/InsertPictureActivity.kt
@@ -3,43 +3,52 @@ package com.example.videoeditpoc
 import android.Manifest
 import android.app.Activity
 import android.app.ProgressDialog
-import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.os.Handler
-import android.os.Looper
 import android.provider.MediaStore
+import android.util.Base64
 import android.util.Log
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.lifecycleScope
+import androidx.media3.common.Effect
+import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
-import com.arthenica.ffmpegkit.FFmpegKit
-import com.arthenica.ffmpegkit.FFmpegKitConfig
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.effect.BitmapOverlay
+import androidx.media3.effect.OverlayEffect
+import androidx.media3.effect.OverlaySettings
+import androidx.media3.effect.TextureOverlay
+import androidx.media3.transformer.Composition
+import androidx.media3.transformer.EditedMediaItem
+import androidx.media3.transformer.Effects
+import androidx.media3.transformer.ExportException
+import androidx.media3.transformer.ExportResult
+import androidx.media3.transformer.TransformationRequest
+import androidx.media3.transformer.Transformer
 import com.example.videoeditpoc.databinding.ActivityInsertPictureBinding
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+import com.google.common.collect.ImmutableList
+import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.FileInputStream
-import java.io.IOException
-import java.io.InputStream
+import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.Date
-import kotlin.math.abs
+import kotlin.math.roundToInt
 
+
+@UnstableApi
 class InsertPictureActivity : AppCompatActivity() {
     lateinit var binding: ActivityInsertPictureBinding
-    private var input_video_uri_ffmpeg: String? = null
-    val handler = Handler(Looper.getMainLooper())
+    private var input_video_uri: String? = null
     private var imageUri: Uri? = null
-    var imageFilePath: String? = null
     private var outputFilePath: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,8 +56,7 @@ class InsertPictureActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.selectVideoBtn.setOnClickListener {
-            handler.removeCallbacksAndMessages(null)
-            selectVideoLauncherUsingFfmpeg.launch("video/*")
+            selectVideoLauncher.launch("video/*")
         }
 
         binding.insertEmojiActBtn.setOnClickListener {
@@ -57,7 +65,7 @@ class InsertPictureActivity : AppCompatActivity() {
 
         binding.insertPictureBtn.setOnClickListener {
 
-            if (input_video_uri_ffmpeg == null) {
+            if (input_video_uri == null) {
                 Toast.makeText(this, "Select video", Toast.LENGTH_SHORT).show()
                 return@setOnClickListener
             }
@@ -78,18 +86,6 @@ class InsertPictureActivity : AppCompatActivity() {
             }
         }
 
-//        binding.saveVideo.setOnClickListener {
-//            if (input_video_uri_ffmpeg != null) {
-//                //passing filename
-//                saveVideoLauncher.launch("VID-${System.currentTimeMillis() / 1000}")
-//            } else Toast.makeText(
-//                this@InsertPictureActivity, "Please upload video", Toast.LENGTH_LONG
-//            ).show()
-//        }
-
-//        binding.reverseVideoBtnBtn.setOnClickListener {
-//            startActivity(Intent(this, MainActivity::class.java))
-//        }
     }
 
     private var imageLauncher =
@@ -97,35 +93,57 @@ class InsertPictureActivity : AppCompatActivity() {
             if (result.resultCode == Activity.RESULT_OK) {
                 imageUri = result.data!!.data
                 Log.d("imageUri", "imageUri: $imageUri")
-                if (imageUri != null && "content" == imageUri!!.scheme) {
-                    val cursor = this.contentResolver.query(
-                        imageUri!!, arrayOf(MediaStore.Images.ImageColumns.DATA), null, null, null
-                    )
-                    cursor!!.moveToFirst()
-                    imageFilePath = cursor.getString(0)
-                    Log.d("imageFilePathScheme", "imageFilePath: $imageFilePath")
-                    cursor.close()
-                } else {
-                    imageFilePath = imageUri!!.path
-                    Log.d("imageFilePathwoScheme", "imageFilePathwosc: $imageFilePath ")
-                }
-                Log.d("", "Chosen path = $imageFilePath")
-                Log.d("imagema", "image : $imageUri")
-//                val folder = cacheDir
-//                val file = File(folder, System.currentTimeMillis().toString() + ".mp4")
-
-                outputFilePath = getOutputFilePath()
-                val command =
-                    "-y -i $input_video_uri_ffmpeg -i ${
-                        Uri.fromFile(
-                            File(
-                                imageFilePath!!
-                            )
+                val bitmap: Bitmap = if (Build.VERSION.SDK_INT >= 29) {
+                    // To handle deprecation use
+                    ImageDecoder.decodeBitmap(
+                        ImageDecoder.createSource(
+                            contentResolver,
+                            imageUri!!
                         )
-                    } -filter_complex \"[0:v][1:v] overlay=25:25:enable='between(t,0,20)'\" -pix_fmt yuv420p -c:a copy $outputFilePath"
-                executeFfmpegCommand(command, outputFilePath!!)
+                    )
+                } else {
+                    // Use older version
+                    MediaStore.Images.Media.getBitmap(contentResolver, imageUri)
+                }
+
+                Toast.makeText(this, "bitmap: $bitmap", Toast.LENGTH_LONG).show()
+
+                val scaledBitmap = scaleDownImage(bitmap, maxImageHeight = 96f)
+                Toast.makeText(this, "scaledBitmap:$scaledBitmap", Toast.LENGTH_LONG).show()
+                val resultantBitmapFilePath = saveBitmap(scaledBitmap)
+                Toast.makeText(this, "path: $resultantBitmapFilePath", Toast.LENGTH_LONG).show()
+                val mediaItem = MediaItem.Builder().setUri(input_video_uri).build()
+                createTransformation(mediaItem, resultantBitmapFilePath)
             }
         }
+
+    private fun encodeImage(bm: Bitmap): String? {
+        val baos = ByteArrayOutputStream()
+        bm.compress(Bitmap.CompressFormat.PNG, 100, baos)
+        val b = baos.toByteArray()
+        return Base64.encodeToString(b, Base64.DEFAULT)
+    }
+
+    private fun scaleDownImage(
+        realImage: Bitmap, maxImageHeight: Float
+    ): Bitmap {
+        return if (realImage.height <= maxImageHeight) {
+            realImage
+        } else {
+
+            val ratio = Math.min(
+                maxImageHeight / realImage.width,
+                maxImageHeight / realImage.height
+            )
+            val width = (ratio * realImage.width).roundToInt()
+            val height = (ratio * realImage.height).roundToInt()
+
+            Bitmap.createScaledBitmap(
+                realImage, width,
+                height, true
+            )
+        }
+    }
 
     override fun onRequestPermissionsResult(
         requestCode: Int, permissions: Array<out String>, grantResults: IntArray
@@ -149,7 +167,7 @@ class InsertPictureActivity : AppCompatActivity() {
                         }
                     } else {
 
-                        if (input_video_uri_ffmpeg != null) {
+                        if (input_video_uri != null) {
                             insertImage()
                         } else {
                             Toast.makeText(
@@ -165,103 +183,23 @@ class InsertPictureActivity : AppCompatActivity() {
         }
     }
 
-    private val selectVideoLauncherUsingFfmpeg =
+    private val selectVideoLauncher =
         registerForActivityResult(ActivityResultContracts.GetContent()) {
             it?.let {
-                input_video_uri_ffmpeg = FFmpegKitConfig.getSafParameterForRead(this, it)
+                input_video_uri = it.toString()
                 Toast.makeText(
                     this,
-                    "video loaded successfully: $input_video_uri_ffmpeg",
+                    "video loaded successfully: $input_video_uri",
                     Toast.LENGTH_SHORT
                 ).show()
             }
         }
-
-//    private val saveVideoLauncher =
-//        registerForActivityResult(ActivityResultContracts.CreateDocument("video/mp4")) {
-//            it?.let {
-//                val out = contentResolver.openOutputStream(it)
-//                val ip: InputStream = FileInputStream(input_video_uri_ffmpeg)
-//
-//                //com.google.common.io.ByteStreams, also provides a direct method to copy
-//                // all bytes from the input stream to the output stream. Does not close or
-//                // flush either stream.
-//                // copy(ip,out!!)
-//
-//                out?.let {
-//                    val buffer = ByteArray(1024)
-//                    var read: Int
-//                    while (ip.read(buffer).also { read = it } != -1) {
-//                        out.write(buffer, 0, read)
-//                    }
-//                    ip.close()
-//                    // write the output file (You have now copied the file)
-////                    out.flush()
-//                    out.close()
-//                }
-//            }
-//        }
 
 
     private fun insertImage() {
         val intent = Intent(Intent.ACTION_PICK)
         intent.type = "image/*"
         imageLauncher.launch(intent)
-    }
-
-    private fun executeFfmpegCommand(exe: String, filePath: String) {
-
-        //creating the progress dialog
-        val progressDialog = ProgressDialog(this@InsertPictureActivity)
-        progressDialog.setCancelable(false)
-        progressDialog.setCanceledOnTouchOutside(false)
-        progressDialog.show()
-
-        /*
-            Here, we have used he Async task to execute our query because if we use the regular method the progress dialog
-            won't be visible. This happens because the regular method and progress dialog uses the same thread to execute
-            and as a result only one is a allowed to work at a time.
-            By using we Async task we create a different thread which resolves the issue.
-         */
-        FFmpegKit.executeAsync(exe, { session ->
-            val returnCode = session.returnCode
-            lifecycleScope.launch(Dispatchers.Main) {
-                if (returnCode.isValueSuccess) {
-                    //after successful execution of ffmpeg command,
-                    //again set up the video Uri in VideoView
-                    //change the video_url to filePath, so that we could do more manipulations in the
-                    //resultant video. By this we can apply as many effects as we want in a single video.
-                    //Actually there are multiple videos being formed in storage but while using app it
-                    //feels like we are doing manipulations in only one video
-                    outputFilePath = filePath
-                    //play the result video in VideoView
-                    progressDialog.dismiss()
-                    Toast.makeText(this@InsertPictureActivity, "Filter Applied", Toast.LENGTH_SHORT)
-                        .show()
-                } else {
-                    progressDialog.dismiss()
-                    Log.d("TAG", session.allLogsAsString)
-                    Toast.makeText(
-                        this@InsertPictureActivity, "Something Went Wrong!", Toast.LENGTH_SHORT
-                    ).show()
-                }
-            }
-        }, { log ->
-            lifecycleScope.launch(Dispatchers.Main) {
-                progressDialog.setMessage("Applying Filter..${log.message}")
-            }
-        }) { statistics ->
-            Log.d("STATS", statistics.toString())
-
-        }
-    }
-
-    @Throws(IOException::class)
-    private fun createExternalCacheFile(fileName: String): File {
-        val file = File(externalCacheDir, fileName)
-        check(!(file.exists() && !file.delete())) { "Could not delete the previous export output file" }
-        check(file.createNewFile()) { "Could not create the export output file" }
-        return file
     }
 
     private fun getOutputFilePath(): String? {
@@ -286,5 +224,99 @@ class InsertPictureActivity : AppCompatActivity() {
 
 
         return file.absolutePath
+    }
+
+    private fun saveBitmap(bitmap: Bitmap): String? {
+        val currentTimeMillis = System.currentTimeMillis()
+        val today = Date(currentTimeMillis)
+        val dateFormat = SimpleDateFormat("yyyyMMdd_HHmmss")
+        val fileName: String = "bitmap_" + dateFormat.format(today) + ".png"
+        val bitmapDirectory =
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).absoluteFile
+        if (!bitmapDirectory.exists()) {
+            bitmapDirectory.mkdir()
+        }
+        val file = File(bitmapDirectory, fileName)
+        file.createNewFile()
+        println("No bitmap file found bitmap file created ${file.absolutePath}")
+
+        try {
+            val out = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
+            out.flush()
+            out.close()
+            Toast.makeText(this, "file with bitmap saved", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return file.absolutePath
+    }
+
+    private fun createVideoEffects(imageFilePath: String?): ImmutableList<Effect> {
+        val effects = ImmutableList.Builder<Effect>()
+        val overlayEffect: OverlayEffect = createOverlayEffect(imageFilePath)!!
+        effects.add(overlayEffect)
+        return effects.build()
+    }
+
+    private fun createOverlayEffect(imageFilePath: String?): OverlayEffect? {
+        val overLaysBuilder: ImmutableList.Builder<TextureOverlay> = ImmutableList.builder()
+        val overlaySettings = OverlaySettings.Builder().build()
+        val imageFilePathUri = Uri.fromFile(File(imageFilePath!!))
+        val imageOverlay =
+            BitmapOverlay.createStaticBitmapOverlay(this, imageFilePathUri, overlaySettings)
+        overLaysBuilder.add(imageOverlay)
+
+
+        val overlays: ImmutableList<TextureOverlay> = overLaysBuilder.build()
+        return if (overlays.isEmpty()) null else OverlayEffect(overlays)
+    }
+
+    private fun createTransformation(mediaItem: MediaItem, imageFilePath: String?) {
+
+        val inputEditedMediaItem = EditedMediaItem.Builder(mediaItem).setEffects(
+            Effects(listOf(), createVideoEffects(imageFilePath))
+        ).build()
+        val transformer = transformerBuilder()
+        outputFilePath = getOutputFilePath()
+        Log.d("ita", "createTransformation:$outputFilePath")
+        transformer.start(inputEditedMediaItem, outputFilePath!!)
+    }
+
+    private fun transformerBuilder(): Transformer {
+
+
+        val progressDialog = ProgressDialog(this@InsertPictureActivity)
+        progressDialog.setCancelable(false)
+        progressDialog.setMessage("Applying Filter..")
+        progressDialog.setCanceledOnTouchOutside(false)
+        progressDialog.show()
+        val request = TransformationRequest.Builder().setVideoMimeType(MimeTypes.VIDEO_H264)
+            .setAudioMimeType(MimeTypes.AUDIO_AAC).build()
+        val transformerListener: Transformer.Listener = object : Transformer.Listener {
+            override fun onCompleted(composition: Composition, result: ExportResult) {
+                Log.d("vcas", "success")
+
+                progressDialog.dismiss()
+                Toast.makeText(this@InsertPictureActivity, "Filter Applied", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+            override fun onError(
+                composition: Composition, result: ExportResult, exception: ExportException
+            ) {
+                Log.d("vcae", "fail")
+                progressDialog.dismiss()
+                Toast.makeText(
+                    this@InsertPictureActivity,
+                    "Something went wrong",
+                    Toast.LENGTH_SHORT
+                )
+                    .show()
+            }
+        }
+        return Transformer.Builder(this).setTransformationRequest(request)
+            .addListener(transformerListener).build()
     }
 }
